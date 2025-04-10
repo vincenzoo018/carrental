30de5d55be6b21a785d211e9cca755976e632d80
vincenzoo018 <v.toylo.546983@umindanao.edu.ph>
2025-04-10 23:19:28 +0800

    commtting

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\ServiceController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/booking', [BookingController::class, 'index'])->name('booking');
+Route::get('/cars', [CarController::class, 'index'])->name('cars');
+Route::get('/reservation', [ReservationController::class, 'index'])->name('reservation');
+Route::get('/services', [ServiceController::class, 'index'])->name('services');
